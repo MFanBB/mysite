5ce47ae4aa1e2bccb37cc894ec5bfbebee90d343
zhangmoumou1 <932545014@qq.com>
2018-08-21 15:10:28 +0800

    1

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('comment/', include('comment.urls')),
     path('likes/', include('likes.urls')),
     path('user/', include('user.urls')),
-    path('upload/', image_upload),
+    path('upload/', views.image_upload),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
